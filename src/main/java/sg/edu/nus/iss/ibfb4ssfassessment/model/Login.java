42bb17e3c6432b4ca734217ae62cc5bac06af3b1
Jo-cloud85 <joan_tta@live.com.sg>
2024-03-27 15:14:17 +0800

    minor change

--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties.Simple;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.validation.constraints.Email;
